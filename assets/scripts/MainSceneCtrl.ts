c068645360c6ea23c9f67f4d5e91c64d52c29963
caizhitao <czt.saisam@gmail.com>
2019-07-09 21:32:17 +0800

    更新主场景

--- assets/scripts/MainSceneCtrl.ts
+++ assets/scripts/MainSceneCtrl.ts
@@ -2,10 +2,7 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class MainSceneCtrl extends cc.Component {
-	@property(cc.Label)
-	label: cc.Label = null;
-
-	start() {
+	onLoad() {
 		cc.error("进入主场景");
 	}
 }
