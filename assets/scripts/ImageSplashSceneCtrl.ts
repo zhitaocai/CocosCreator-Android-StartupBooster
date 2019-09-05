379daea0c96921fe094d50685903f40825c27c8a
caizhitao <czt.saisam@gmail.com>
2019-09-05 21:41:03 +0800

    加入主场景为图片的示例

--- assets/scripts/ImageSplashSceneCtrl.ts
+++ assets/scripts/ImageSplashSceneCtrl.ts
@@ -1,10 +1,7 @@
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class MainSceneCtrl extends cc.Component {
-	onLoad() {
-		cc.error("进入主场景");
-	}
+export default class ImageSplashSceneCtrl extends cc.Component {
 
 	start() {
 		this.scheduleOnce(() => {
